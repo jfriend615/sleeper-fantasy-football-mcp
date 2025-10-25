70c1e26398cd81c73490dc5ecee3100af03a5665
jfriend615 <jordanfreund93@gmail.com>
2025-10-24 20:46:20 -0400

    Enable external directory support

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    externalDir: true,
     serverComponentsExternalPackages: ['@modelcontextprotocol/sdk'],
   },
 }
